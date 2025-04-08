d0181d52f8a719ad028211fefd3b11baa298f96b
Anand Dewangan <118416821+AnandDewangan@users.noreply.github.com>
2025-04-08 10:05:37 +0530

    Add files via upload

--- src/pages/PricingTable.js
+++ src/pages/PricingTable.js
@@ -5,63 +5,123 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 const PricingTable = () => {
   const packages = [
     {
-      name: "Silver",
-      price: "₹5,999/-",
+      name: "CLASSIC",
+      price: "₹4,999/-",
       icon: "bi-book-fill",
       features: [
-        ["Paperback + eBook Publishing", "Non-Exclusive Contract"],
-        ["Dedicated Project Manager", "B&W Book (Colored Cover)"],
-        ["Customised Cover Design", "Customised Pagination + Typesetting"],
-        ["Digital Proof", "ISBN + Bar Code"],
-        ["e-Book Conversion", "Global eBook Distribution"],
-        ["Paperback Distribution India", "Unlimited Inventory Management"],
-        ["10 Complimentary Copies", "Social Media Templates"],
-        ["Publishing E-Certificate", "Annual Sales Reporting"],
-        ["(100%) Royalty Payment", "Post Publishing Support"],
+        ["Paperback + E-Book Publishing", "Author Contract"],
+        ["Publishing Duration – 20-25 Days", "Author Copyright"],
+        [
+          "Including Pages (Up-To 180 Pages Regular Size)",
+          "Dedicated Project Manager",
+        ],
+        ["B&W Book (Colored Cover)", "Cover Design (Template Based)"],
+        ["Interior Design ", "ISBN + Bar Code Assignment"],
+        ["Digital Proof", "Publishing E-Certificate"],
+        [
+          "Author Dashboard (Check Live Sales Report)",
+          "Social Media Templates ",
+        ],
+        ["Basic Marketing (1 Weak)", "(100%) Royalty Payment"],
+        [
+          "Unlimited Inventory Management (Print On Demand Service)",
+          "Distribution To Major Platforms ( Amazon, Flipkart And Google Books Etc)",
+        ],
       ],
       missingFeatures: [
-        ["Extensive Editing & Proof Reading", "B&W Illustrations"],
-        ["Amazon Ad Campaign", "Basic Marketing"],
-        ["Author Interview"]
+        ["Custom Cover Design", "Pagination + Typesetting"],
+        ["Amazon Prime listing", "Basic Marketing"],
+        ["Book trailer video", ""],
       ],
     },
     {
-      name: "Gold",
+      name: "SUPREME",
       price: "₹7,999",
       icon: "bi-file-earmark-pdf-fill",
       features: [
-        ["Paperback + eBook Publishing", "Non-Exclusive Contract"],
-        ["Dedicated Project Manager", "B&W Book (Colored Cover)"],
-        ["Extensive Editing & Proof Reading", "Customised Cover Design"],
-        ["Customised Pagination + Typesetting", "Up to 20 B&W Illustrations"],
-        ["Digital Proof", "ISBN + Bar Code"],
-        ["e-Book Conversion", "Global eBook Distribution"],
-        ["Paperback Distribution India", "Unlimited Inventory Management"],
-        ["20 Complimentary Copies", "Amazon Ad Campaign"],
-        ["Social Media Templates", "Publishing Hard Copy Certificate"],
-        ["Annual Sales Reporting", "(100%) Royalty Payment"],
-        ["Post Publishing Support"]
+        ["Paperback + E-Book Publishing", "Author Contract"],
+        ["Publishing Duration – 15-20 Days", "Author Copyright"],
+        [
+          "Including Pages (Up-To 270 Pages Regular Size)",
+          "Dedicated Project Manager",
+        ],
+        [
+          "B&W Book (Colored Cover)",
+          "Custom Cover Design (2 Concept+3 Revisions)",
+        ],
+        [
+          "Interior Design (Pagination + Typesetting)",
+          "ISBN + Bar Code Assignment",
+        ],
+        ["Digital Proof", "Publishing E-Certificate"],
+        [
+          "Author Dashboard (Check Live Sales Report)",
+          "Social Media Templates ",
+        ],
+        ["Basic Marketing (1 Weak)", "(100%) Royalty Payment"],
+        [
+          "Amazon Prime listing- 1 Month",
+          "Basic Marketing (15 Days Instagram and Facebook )",
+        ],
+        [
+          "Book trailer video (Only One Concept & 1 Revisions)",
+          "Publishing E-Certificate",
+        ],
+        [
+          "Unlimited Inventory Management (Print On Demand Service)",
+          "Distribution To Major Platforms ( Amazon, Flipkart, Kindle And Google Books Etc)",
+        ],
       ],
-      missingFeatures: [["Influencer Marketing", "Author Interview"]],
+      missingFeatures: [
+        ["Extensive Editing & Proofreading", "Govt. Copyright"],
+        ["B&W Book & Illustration", "Listing On Global ebook and Paperback Distribution ( Amazon, Flipkart, Amazon kindle and Google Books Etc)"],
+        ["Amazon Prime Listing - 1 Year", "Customised Cover Design (Unlimited Concept+ Unlimited Revisions)"]
+      ],
     },
     {
-      name: "Platinum",
+      name: "PLATINUM",
       price: "₹11,999",
       icon: "bi-file-earmark-pdf-fill",
       features: [
-        ["Paperback + eBook Publishing", "Non-Exclusive Contract"],
-        ["Dedicated Project Manager", "B&W Book (Colored Cover)"],
-        ["Extensive Editing & Proof Reading", "Customised Cover Design"],
-        ["Customised Pagination + Typesetting", "B&W Illustrations"],
-        ["Digital Proof", "ISBN + Bar Code"],
-        ["e-Book Conversion", "Global eBook Distribution"],
-        ["Global Paperback Distribution", "Unlimited Inventory Management"],
-        ["30 Complimentary Copies", "Amazon Ad Campaign"],
-        ["Influencer Marketing", "Social Media Templates"],
-        ["Publishing Hard Copy Certificate", "Annual Sales Reporting"],
-        ["(100%) Royalty Payment", "Post Publishing Support"]
+        ["Paperback + E-Book Publishing", "Author Contract"],
+        ["Publishing Duration - 7-10 Days", "Author Copyright"],
+        [
+          "Including Pages (Up-To 370 Pages Regular Size)",
+          "Dedicated Project Manager",
+        ],
+        ["Extensive Editing & Proofreading ", "Govt. Copyright"],
+        [
+          "B&W Book & Illustration",
+          "Customised Cover Design (Unlimited Concept+ Unlimited Revisions)",
+        ],
+        [
+          "Interior Design (Customised Pagination + Typesetting)",
+          "ISBN + Bar Code Assignment",
+        ],
+        ["Digital Proof", "Publishing E-Certificate"],
+        [
+          "Author Dashboard (Check Live Sales Report)",
+          "Social Media Templates ",
+        ],
+        ["Basic Marketing (1 Weak)", "(100%) Royalty Payment"],
+        [
+          "Amazon Prime Listing-1Year",
+          "Book Marketing (30 Days Instagram and Facebook Ads)",
+        ],
+        [
+          "Book trailer video (Only One Concept & 1 Revisions)",
+          "Publishing E-Certificate",
+        ],
+        [
+          "Unlimited Inventory Management (Print On Demand Service)",
+          "Distribution To Major Platforms ( Amazon, Flipkart, Kindle And Google Books Etc)",
+        ],
+        [
+          "Listing On Global ebook and Paperback Distribution ( Amazon, Flipkart, Amazon kindle and Google Books Etc)",
+          "Custom Book Trailer Video",
+        ],
       ],
-      missingFeatures: [["Author Interview"]],
+      missingFeatures: [],
     },
   ];
 
@@ -93,8 +153,9 @@ const PricingTable = () => {
       <div className="section-title text-center mb-5">
         <h2 className="fs-2 text-danger">Pricing</h2>
         <h6 className="text-muted">
-          Find your best prices of the books premium packages that are available with us. You can purchase the
-          prices and get the discount of the purchase of the books.
+          Find your best prices of the books premium packages that are available
+          with us. You can purchase the prices and get the discount of the
+          purchase of the books.
         </h6>
       </div>
       <div className="row">
@@ -104,7 +165,9 @@ const PricingTable = () => {
               <a
                 key={index}
                 className={`nav-link text-dark border rounded mb-2 p-3 shadow-sm ${
-                  activeSection === `package-${index}` ? "active bg-danger text-white" : ""
+                  activeSection === `package-${index}`
+                    ? "active bg-danger text-white"
+                    : ""
                 }`}
                 href={`#package-${index}`}
               >
@@ -115,7 +178,11 @@ const PricingTable = () => {
         </div>
         <div className="col-md-8">
           {packages.map((pkg, index) => (
-            <div id={`package-${index}`} className="card shadow-lg mb-4" key={index}>
+            <div
+              id={`package-${index}`}
+              className="card shadow-lg mb-4"
+              key={index}
+            >
               <div className="card-body">
                 <h2 className="card-title text-danger">
                   {pkg.name} <i className={`bi ${pkg.icon}`}></i>
@@ -123,21 +190,42 @@ const PricingTable = () => {
                 <p className="price display-6">{pkg.price}</p>
                 <ul className="list-unstyled">
                   {pkg.features.map((featurePair, i) => (
-                    <li key={i} className="text-success d-flex align-items-center">
+                    <li
+                      key={i}
+                      className="text-success d-flex align-items-center"
+                    >
                       <i className="bi bi-check-circle-fill me-2"></i>
-                      <span className="text-break text-wrap w-50">{featurePair[0]}</span>
-                      <span className="text-break text-wrap w-50">{featurePair[1]}</span>
+                      <span className="text-break text-wrap w-50">
+                        {featurePair[0]}
+                      </span>
+                      <span className="text-break text-wrap w-50">
+                        {featurePair[1]}
+                      </span>
                     </li>
                   ))}
                   {pkg.missingFeatures.map((featurePair, i) => (
-                    <li key={i} className="text-danger d-flex align-items-center">
+                    <li
+                      key={i}
+                      className="text-danger d-flex align-items-center"
+                    >
                       <i className="bi bi-x-circle-fill me-2"></i>
-                      <span className="text-break text-wrap w-50">{featurePair[0]}</span>
-                      {featurePair[1] && <span className="text-break text-wrap w-50">{featurePair[1]}</span>}
+                      <span className="text-break text-wrap w-50">
+                        {featurePair[0]}
+                      </span>
+                      {featurePair[1] && (
+                        <span className="text-break text-wrap w-50">
+                          {featurePair[1]}
+                        </span>
+                      )}
                     </li>
                   ))}
                 </ul>
-                  <a href="tel: +91 9171242297" className="btn btn-outline-success btn-lg"><i className="bi bi-telephone-fill"></i> Call Now</a> 
+                <a
+                  href="tel: +919171242297"
+                  className="btn btn-outline-success btn-lg"
+                >
+                  <i className="bi bi-telephone-fill"></i> Call Now
+                </a>
               </div>
             </div>
           ))}
