8c72eb48c68fe833c83f7bd39e8e9e79c143c3b1
Anand Dewangan <118416821+AnandDewangan@users.noreply.github.com>
2025-04-17 14:39:58 +0530

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,7 +1,47 @@
+const CACHE_NAME = 'v1';
+const CACHE_URLS = [
+  '/',
+  '/index.html',
+  '/static/js/main.js',
+  '/static/css/main.css',
+  '/static/media/logo.5e350eac0a92c9b8a15f.png',
+  // add other files you want to cache for offline usage
+];
+
 self.addEventListener("install", (event) => {
   console.log("Service Worker installing.");
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Caching assets");
+      return cache.addAll(CACHE_URLS);
+    })
+  );
 });
 
 self.addEventListener("activate", (event) => {
   console.log("Service Worker activating.");
+  // Optionally clean up old caches
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log("Deleting old cache", cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  console.log("Fetching:", event.request.url);
+
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      // Return cached response if available, else fetch from network
+      return cachedResponse || fetch(event.request);
+    })
+  );
 });
