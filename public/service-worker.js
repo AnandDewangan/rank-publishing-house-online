24d116587c8047432b44f8528074b2c4a8902405
Anand Dewangan <118416821+AnandDewangan@users.noreply.github.com>
2025-04-18 13:08:26 +0530

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -44,7 +44,6 @@ self.addEventListener("activate", (event) => {
 });
 
 self.addEventListener("fetch", (event) => {
-  // console.log("Fetching:", event.request.url);
 
   event.respondWith(
     caches.match(event.request).then((cachedResponse) => {
